06228c0128670ff9b0ace8558acbac9eba995a54
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 15:16:17 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+//Dinner guest: working with one of the programsfrom exercise 14 through 18, print a message indicating the number of 
+//people you are inviting to dinner.
+// Solution # 19
+
+let invitations: string[] = ['Saad', 'Shahzad'];
+let count_invitations: number = invitations.length
+console.log(`${count_invitations} people will come to the dinner`)
